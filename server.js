9e79ab08b7a586211a419623c7ad8f31151a7742
xy-gt_ghvfvv <pawapareek506@gmail.com>
2024-02-26 13:35:58 +0530

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { routes } from './routes/index.js';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 
 // Load environment variables from .env file
 dotenv.config();
@@ -17,6 +18,7 @@ const port = process.env.PORT || 3000;
 // app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 // app.use("/api/admin" , require("./routes/adminRoute"))
 app.use("/api", routes)
 
